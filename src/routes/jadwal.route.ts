7bffd43215e19030232398580876ea6c3b4c2141
Gindra-o7 <gilangindra734@gmail.com>
2025-07-11 09:45:02 +0700

    feat: add jadwal route with CRUD operations and authentication middleware

---
+++ src/routes/jadwal.route.ts
@@ -0,0 +1,19 @@
+import { Hono } from "hono";
+import { RegExpRouter } from "hono/router/reg-exp-router";
+import { zodError } from "../utils/zod-error.util";
+import { zValidator } from "@hono/zod-validator";
+import JadwalHandler from "../handlers/jadwal.handler";
+import AuthMiddleware from "../middlewares/auth.middleware";
+import { postJadwalSchema } from "../validators/jadwal.validator";
+
+const jadwalRoute = new Hono({ router: new RegExpRouter() });
+
+jadwalRoute.use("/jadwal/*", AuthMiddleware.JWTBearerTokenExtraction);
+
+jadwalRoute.get("/jadwal/me", JadwalHandler.getMe);
+jadwalRoute.get("/jadwal", JadwalHandler.getAll);
+jadwalRoute.get("/jadwal/:id", JadwalHandler.get);
+jadwalRoute.post("/jadwal", zValidator("json", postJadwalSchema, zodError), JadwalHandler.post);
+jadwalRoute.put("/jadwal/:id", zValidator("json", postJadwalSchema, zodError), JadwalHandler.put);
+
+export default jadwalRoute;
