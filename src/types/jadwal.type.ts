1a71ae9c66e095804e13be05f742086ce385cef2
Gindra-o7 <gilangindra734@gmail.com>
2025-07-11 09:45:42 +0700

    feat: add PostJadwalType interface for jadwal creation

---
+++ src/types/jadwal.type.ts
@@ -0,0 +1,16 @@
+import { JenisJadwal } from "../generated/prisma";
+
+export interface PostJadwalType {
+  tanggal: Date;
+  waktu_mulai: Date;
+  waktu_selesai: Date;
+  jenis: JenisJadwal;
+  nim: string;
+  nama_ruangan: string;
+  nip_pembimbing_1: string;
+  nip_pembimbing_2?: string;
+  nip_penguji_1: string;
+  nip_penguji_2?: string;
+  nip_ketua_sidang: string;
+  kode_tahun_ajaran: string;
+}
