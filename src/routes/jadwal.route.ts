be25a47ba02e2301a0355c733188283fd3b9871a
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-07-17 14:34:31 +0700

    fix: add jadwal route

--- src/routes/jadwal.route.ts
+++ src/routes/jadwal.route.ts
@@ -10,7 +10,7 @@ const jadwalRoute = new Hono({ router: new RegExpRouter() });
 
 jadwalRoute.use("/jadwal/*", AuthMiddleware.JWTBearerTokenExtraction);
 
-jadwalRoute.get("/jadwal/me", JadwalHandler.getMe);
+jadwalRoute.get("/me/jadwal", JadwalHandler.getMe);
 jadwalRoute.get("/jadwal", JadwalHandler.getAll);
 jadwalRoute.get("/jadwal/:id", JadwalHandler.get);
 jadwalRoute.post("/jadwal", zValidator("json", postJadwalSchema, zodError), JadwalHandler.post);
