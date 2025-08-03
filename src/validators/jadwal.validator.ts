9a3e63256e225d9b92ac23bb9de78ecd0e14f9fa
Gindra-o7 <gilangindra734@gmail.com>
2025-08-04 00:33:17 +0700

    ⚙refactor: rename postJadwalSchema to postPutJadwalSchema for consistency and enhance validation logic

--- src/validators/jadwal.validator.ts
+++ src/validators/jadwal.validator.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 import { JenisJadwal } from "../generated/prisma";
 
-export const postJadwalSchema = z.object({
+export const postPutJadwalSchema = z.object({
   tanggal: z.string().refine((val) => !isNaN(Date.parse(val)), {
     message: "Format tanggal tidak valid",
   }),
@@ -19,4 +19,8 @@ export const postJadwalSchema = z.object({
   nip_penguji_1: z.string(),
   nip_penguji_2: z.string().optional(),
   nip_ketua_sidang: z.string(),
-});
+}).refine((data) => new Date(data.waktu_selesai) > new Date(data.waktu_mulai), {
+		message: "Waktu selesai tidak boleh lebih awal dari waktu mulai",
+		path: ["waktu_selesai"],
+  })
+;
