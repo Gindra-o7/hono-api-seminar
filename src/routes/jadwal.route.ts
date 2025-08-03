06cd1fd3724c33aa7ceda876f306ba5073a33204
Gindra-o7 <gilangindra734@gmail.com>
2025-08-04 00:33:02 +0700

    ⚙refactor: update jadwal route to use postPutJadwalSchema and correct route paths for improved consistency

--- src/routes/jadwal.route.ts
+++ src/routes/jadwal.route.ts
@@ -4,16 +4,16 @@ import { zodError } from "../utils/zod-error.util";
 import { zValidator } from "@hono/zod-validator";
 import JadwalHandler from "../handlers/jadwal.handler";
 import AuthMiddleware from "../middlewares/auth.middleware";
-import { postJadwalSchema } from "../validators/jadwal.validator";
+import { postPutJadwalSchema } from "../validators/jadwal.validator";
 
 const jadwalRoute = new Hono({ router: new RegExpRouter() });
 
-jadwalRoute.use("/jadwal/*", AuthMiddleware.JWTBearerTokenExtraction);
+jadwalRoute.use("/jadwal*", AuthMiddleware.JWTBearerTokenExtraction);
 
-jadwalRoute.get("/me/jadwal", JadwalHandler.getMe);
+jadwalRoute.get("/jadwal/me", JadwalHandler.getMe);
 jadwalRoute.get("/jadwal", JadwalHandler.getAll);
 jadwalRoute.get("/jadwal/:id", JadwalHandler.get);
-jadwalRoute.post("/jadwal", zValidator("json", postJadwalSchema, zodError), JadwalHandler.post);
-jadwalRoute.put("/jadwal/:id", zValidator("json", postJadwalSchema, zodError), JadwalHandler.put);
+jadwalRoute.post("/jadwal", zValidator("json", postPutJadwalSchema, zodError), JadwalHandler.post);
+jadwalRoute.put("/jadwal/:id", zValidator("json", postPutJadwalSchema, zodError), JadwalHandler.put);
 
 export default jadwalRoute;
