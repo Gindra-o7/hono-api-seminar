3813173c5084a717f6d9fe76244ed0454292a83d
Gindra-o7 <gilangindra734@gmail.com>
2025-08-03 13:31:19 +0700

    🔥feat: add RuanganHelper class with cekKonflik method for room conflict checking

---
+++ src/helpers/ruangan.helper.ts
@@ -0,0 +1,16 @@
+import prisma from "../infrastructures/db.infrastructure";
+import RuanganRepository from "../repositories/ruangan.repository";
+import { APIError } from "../utils/api-error.util";
+
+export default class RuanganHelper {
+  public static async cekKonflik(
+    kode_ruangan: string, 
+    waktu_mulai: Date, 
+    waktu_selesai: Date
+  ) {
+    const konflik = await RuanganRepository.findKonflik(kode_ruangan, waktu_mulai, waktu_selesai);
+    if (konflik) {
+      throw new APIError(`Ruangan ${kode_ruangan} sudah digunakan pada waktu tersebut`, 409);
+    }
+  }
+}
