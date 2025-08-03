f6a1e40787503a73eec5f7923f6e3acf37a154c6
Gindra-o7 <gilangindra734@gmail.com>
2025-08-03 13:31:01 +0700

    🔥feat: refactor generateId method in JadwalHelper for improved ID generation logic

--- src/helpers/jadwal.helper.ts
+++ src/helpers/jadwal.helper.ts
@@ -1,71 +1,32 @@
 import { JenisJadwal } from "../generated/prisma";
+import DosenRepository from "../repositories/dosen.repository";
 import JadwalRepository from "../repositories/jadwal.repository";
+import RuanganRepository from "../repositories/ruangan.repository";
 import { APIError } from "../utils/api-error.util";
 import TahunAjaranHelper from "./tahun-ajaran.helper";
 
 export default class JadwalHelper {
-  public static generateJadwalId(jenis: JenisJadwal) {
+  // public static generateId(jenis: JenisJadwal) {
+  //   const tahunAjaran = TahunAjaranHelper.findSekarang();
+  //   const uniqueId = crypto.randomUUID();
+  //   return `${jenis}-${tahunAjaran}-${uniqueId}`;
+  // }
+
+  public static async generateId(jenis: JenisJadwal): Promise<string> {
     const tahunAjaran = TahunAjaranHelper.findSekarang();
-    const uniqueId = crypto.randomUUID();
-    return `${jenis}-${tahunAjaran}-${uniqueId}`;
-  }
+    const prefix = `${jenis}-${tahunAjaran}-`;
 
-  public static async cekKonflikRuangan(
-    kode_ruangan: string,
-    waktu_mulai: Date,
-    waktu_selesai: Date
-  ) {
-    const konflik = await JadwalRepository.findKonflikRuangan(
-      kode_ruangan,
-      waktu_mulai,
-      waktu_selesai
-    );
-    if (konflik) {
-      throw new APIError(
-        `Konflik jadwal: Ruangan ${kode_ruangan} sudah digunakan pada waktu tersebut`,
-        409
-      );
-    }
-  }
+    const lastId = await JadwalRepository.findLastId(jenis, tahunAjaran);
 
-  public static async cekKonflikDosen(
-    nips: string[],
-    waktu_mulai: Date,
-    waktu_selesai: Date
-  ) {
-    const konflik = await JadwalRepository.findKonflikDosen(
-      nips,
-      waktu_mulai,
-      waktu_selesai
-    );
-    if (konflik) {
-      throw new APIError(
-        `Konflik jadwal: Salah satu dosen sudah memiliki jadwal pada waktu tersebut`,
-        409
-      );
+    let nextNumber = 1;
+    if (lastId) {
+      const lastNumberStr = lastId.replace(prefix, "");
+      const lastNumber = parseInt(lastNumberStr, 10);
+      if (!isNaN(lastNumber)) {
+        nextNumber = lastNumber + 1;
+      }
     }
-  }
-
-  public static async cekDosen(
-    nip_pembimbing_1: string,
-    nip_pembimbing_2: string | undefined,
-    nip_penguji_1: string,
-    nip_penguji_2: string | undefined
-  ) {
-    const pembimbingNips = [nip_pembimbing_1, nip_pembimbing_2].filter(
-      (nip) => nip !== undefined
-    );
-    const pengujiNips = [nip_penguji_1, nip_penguji_2].filter(
-      (nip) => nip !== undefined
-    );
-    const isConflict = pembimbingNips.some((pembimbing) =>
-      pengujiNips.includes(pembimbing)
-    );
-    if (isConflict) {
-      throw new APIError(
-        `Konflik jadwal: Salah satu dosen sudah menjadi pembimbing dan penguji`,
-        409
-      );
-    }
+    const uniqueId = nextNumber.toString().padStart(3, "0");
+    return `${prefix}${uniqueId}`;
   }
 }
