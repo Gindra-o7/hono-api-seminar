baab819c9737fc6ef961ecd32404601120ffe2e9
Gindra-o7 <gilangindra734@gmail.com>
2025-08-04 00:32:55 +0700

    ⚙refactor: format code in tahun-ajaran.repository.ts for improved readability

--- src/repositories/tahun-ajaran.repository.ts
+++ src/repositories/tahun-ajaran.repository.ts
@@ -2,20 +2,20 @@ import TahunAjaranHelper from "../helpers/tahun-ajaran.helper";
 import prisma from "../infrastructures/db.infrastructure";
 
 export default class TahunAjaranRepository {
-	public static async findAll() {
-		const list_kode_tahun_ajaran = await prisma.jadwal.findMany({
-			select: {
-				kode_tahun_ajaran: true,
-			},
-            orderBy: {
-                kode_tahun_ajaran: "desc",
-            },
-            distinct: ["kode_tahun_ajaran"],        
-		});
+  public static async findAll() {
+    const list_kode_tahun_ajaran = await prisma.jadwal.findMany({
+      select: {
+        kode_tahun_ajaran: true,
+      },
+      orderBy: {
+        kode_tahun_ajaran: "desc",
+      },
+      distinct: ["kode_tahun_ajaran"],
+    });
 
-		return list_kode_tahun_ajaran.map((item) => ({
-			kode: item.kode_tahun_ajaran,
-			nama: TahunAjaranHelper.parseStringNameByCode(item.kode_tahun_ajaran),
-		}));
-	}
+    return list_kode_tahun_ajaran.map((item) => ({
+      kode: item.kode_tahun_ajaran,
+      nama: TahunAjaranHelper.parseStringNameByCode(item.kode_tahun_ajaran),
+    }));
+  }
 }
