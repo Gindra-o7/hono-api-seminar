22c2908bca43f8ec5339edee1e927587fbdf92e5
Gindra-o7 <gilangindra734@gmail.com>
2025-08-03 13:32:15 +0700

    🔥feat: enhance JadwalService methods for improved scheduling conflict checks and data retrieval

--- src/services/jadwal.service.ts
+++ src/services/jadwal.service.ts
@@ -3,84 +3,127 @@ import { APIError } from "../utils/api-error.util";
 import { PostJadwalType } from "../types/jadwal.type";
 import JadwalHelper from "../helpers/jadwal.helper";
 import TahunAjaranHelper from "../helpers/tahun-ajaran.helper";
+import { JenisJadwal } from "../generated/prisma";
+import RuanganHelper from "../helpers/ruangan.helper";
+import DosenHelper from "../helpers/dosen.helper";
 
 export default class JadwalService {
-	public static async getMe(email: string) {
-		const jadwal = await JadwalRepository.findMeByEmail(email);
-		if (!jadwal) {
-			throw new APIError("Jadwal tidak ditemukan", 404);
-		}
-		return {
-			response: true,
-			message: "Data jadwal berhasil diambil",
-			data: jadwal,
-		};
-	}
+  public static async getMe(email: string) {
+    const jadwal = await JadwalRepository.findMeByEmail(email);
+    if (!jadwal) {
+      throw new APIError("Jadwal tidak ditemukan", 404);
+    }
+    return {
+      response: true,
+      message: "Data jadwal berhasil diambil",
+      data: jadwal,
+    };
+  }
 
-	public static async getAll() {
-		const jadwal = await JadwalRepository.findAll();
-		if (!jadwal) {
-			throw new APIError("Jadwal tidak ditemukan", 404);
-		}
-		return {
-			response: true,
-			message: "Data semua jadwal berhasil diambil",
-			data: jadwal,
-		};
-	}
+  public static async getAll(jenis?: JenisJadwal) {
+    const jadwal = await JadwalRepository.findAll(jenis);
+    if (!jadwal) {
+      throw new APIError("Jadwal tidak ditemukan", 404);
+    }
+    return {
+      response: true,
+      message: "Data semua jadwal berhasil diambil",
+      data: jadwal,
+    };
+  }
 
-	public static async get(id: string) {
-		const jadwal = await JadwalRepository.findById(id);
-		if (!jadwal) {
-			throw new APIError("Jadwal tidak ditemukan", 404);
-		}
-		return {
-			response: true,
-			message: "Data jadwal berhasil diambil",
-			data: jadwal,
-		};
-	}
+  public static async get(id: string) {
+    const jadwal = await JadwalRepository.findById(id);
+    if (!jadwal) {
+      throw new APIError("Jadwal tidak ditemukan", 404);
+    }
+    return {
+      response: true,
+      message: "Data jadwal berhasil diambil",
+      data: jadwal,
+    };
+  }
 
-	public static async post(data: PostJadwalType) {
-		await JadwalHelper.cekKonflikRuangan(
-			data.kode_ruangan,
-			new Date(data.waktu_mulai),
-			new Date(data.waktu_selesai)
-		);
+  public static async post(data: PostJadwalType) {
+    await RuanganHelper.cekKonflik(
+      data.kode_ruangan, 
+      new Date(data.waktu_mulai), 
+      new Date(data.waktu_selesai)
+    );
 
-		const nips = [
-			data.nip_pembimbing_1,
-			data.nip_pembimbing_2,
-			data.nip_penguji_1,
-			data.nip_penguji_2,
-			data.nip_ketua_sidang,
-		].filter((nip) => nip !== undefined) as string[];
+    const nips = [
+      data.nip_pembimbing_1, 
+      data.nip_pembimbing_2, 
+      data.nip_penguji_1, 
+      data.nip_penguji_2, 
+      data.nip_ketua_sidang
+    ].filter((nip) => nip !== undefined) as string[];
 
-		await JadwalHelper.cekKonflikDosen(
-			nips,
-			new Date(data.waktu_mulai),
-			new Date(data.waktu_selesai)
-		);
+    await DosenHelper.cekKonflik(
+      nips, 
+      new Date(data.waktu_mulai), 
+      new Date(data.waktu_selesai)
+    );
 
-		const jadwal = await JadwalRepository.create({
-			...data,
-      kode_tahun_ajaran: TahunAjaranHelper.findSekarang(),
-		});
+    await DosenHelper.cekRole(
+      data.nip_pembimbing_1, 
+      data.nip_pembimbing_2, 
+      data.nip_penguji_1, 
+      data.nip_penguji_2
+    );
 
-		return {
-			response: true,
-			message: "Jadwal berhasil ditambahkan",
-			data: jadwal,
-		};
-	}
+    const id = await JadwalHelper.generateId(data.jenis);
+    const kode_tahun_ajaran = TahunAjaranHelper.findSekarang();
 
-	public static async put(id: string, data: PostJadwalType) {
-		await this.get(id);
-		const jadwal = await JadwalRepository.update(id, data);
-		return {
-			response: true,
-			message: "Jadwal berhasil diperbarui",
-			data: jadwal,
-		};
-	}
+    const { id: _, ...dataWithoutId } = data;
+    const jadwal = await JadwalRepository.create({
+      id,
+      ...dataWithoutId,
+      kode_tahun_ajaran,
+    });
+
+    return {
+      response: true,
+      message: "Jadwal berhasil ditambahkan",
+      data: jadwal,
+    };
+  }
+
+  public static async put(id: string, data: PostJadwalType) {
+    await this.get(id);
+
+    await RuanganHelper.cekKonflik(
+      data.kode_ruangan, 
+      new Date(data.waktu_mulai), 
+      new Date(data.waktu_selesai)
+    );
+
+    const nips = [
+      data.nip_pembimbing_1, 
+      data.nip_pembimbing_2, 
+      data.nip_penguji_1, 
+      data.nip_penguji_2, 
+      data.nip_ketua_sidang
+    ].filter((nip) => nip !== undefined) as string[];
+
+    await DosenHelper.cekKonflik(
+      nips, 
+      new Date(data.waktu_mulai), 
+      new Date(data.waktu_selesai)
+    );
+
+    await DosenHelper.cekRole(
+      data.nip_pembimbing_1, 
+      data.nip_pembimbing_2, 
+      data.nip_penguji_1, 
+      data.nip_penguji_2
+    );
+
+    const jadwal = await JadwalRepository.update(id, data);
+    return {
+      response: true,
+      message: "Jadwal berhasil diperbarui",
+      data: jadwal,
+    };
+  }
 }
